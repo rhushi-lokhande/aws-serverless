32d5ab2c14c049df78433366c13e5761bb86a0dc
Rhushikesh Gopalrao Lokhande <rhushikesh.lokhande@lsz.ads.valuelabs.net>
2019-02-22 19:00:13 +0530

    update api to push data in db

--- api/lib/helper.js
+++ api/lib/helper.js
@@ -15,23 +15,43 @@ module.exports.getSaleData = (e,cb)=>{
         return;
     })
 }
-
-module.exports.createData = (e,cb)=>{
-    const param ={
-		TableName: tableName,
-		Item:{
-			id:new Date().getTime(),
-			text:'this is the dummy entry after end',
-			checked:false
-		}
-	}
-	dynamoDb.put(param,(error,result)=>{
-		if(error){
-			cb(error);
-			return;
-		}
-        cb(null,FR(200,param));
-        return;
-	})
+function getRandomizer(bottom, top) {
+    return Math.floor(Math.random() * (1 + top - bottom)) + bottom;
+}
+module.exports.createData = (e, cb) => {
+    var reps = ['John Doe', 'Tom Robert', 'Juilia Cruise', 'Ethan Hunt', 'Micheal Faleti'];
+    var client = ['Google', 'Microsoft', 'Adobe', 'Locusnine', 'Accenture', 'Apple', 'Samsung'];
+    var person = ['Rafael Mafael', 'Marilyn White', 'Lily Wilson', 'Zillah Jacobi', 'Filippa Christensen', 'Ava Burke', 'Viivi lammi', 'Jeff Collins'];
+    var vertical = ['Marketing','Sale','Sr. Management','Technology'];
+    var meeting_location = ['Call','Face to face','Video'];
+    var status1 = ['Lead In','Contact Made','Need Defined','Proposal Made','Negotiation Started','Won','Lost'];
+    var count = 10;
+    for (var i = 0; i <= count; i++) {
+        const param = {
+            TableName: tableName,
+            Item: {
+                id: new Date().getTime(),
+                Rep: reps[getRandomizer(1, 5) - 1],
+                client: client[getRandomizer(1, 7) - 1],
+                contact_person: person[getRandomizer(1, 8) - 1],
+                vertical:vertical[getRandomizer(1, 4) - 1],
+                meeting_location:meeting_location[getRandomizer(1, 3) - 1],
+                status:status1[getRandomizer(1, 7) - 1],
+                MMR:getRandomizer(100, 500),
+                DemoCall:getRandomizer(1, 5),
+                Logo:getRandomizer(1, 5),
+            }
+        }
+        dynamoDb.put(param, (error, result) => {
+            if (error) {
+                cb(error);
+                return;
+            }
+            if (i == count) {
+                cb(null, FR(200, param));
+                return;
+            }
+        })
+    }
 
 }
